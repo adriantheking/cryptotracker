87200040524054c069e0c75207ae7fdc4dc62a5e
Adrian <kiddyphp@gmail.com>
2022-09-17 10:14:20 +0200

    counting worth on front

--- CryptoTracker/ClientApp/src/app/binance/services/binance.service.ts
+++ CryptoTracker/ClientApp/src/app/binance/services/binance.service.ts
@@ -1,9 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import 'rxjs/add/observable/of';
+import { map, share } from 'rxjs/operators';
 import { IWallet } from 'src/app/models/wallet.model';
 import { environment } from 'src/environments/environment';
 import { BinanceTickersModel } from '../models/binanceTickers.model';
+import { Share } from '@ngspot/rxjs/decorators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,7 +24,27 @@ export class BinanceService {
   public GetTickers(): Observable<BinanceTickersModel> {
     return this.httpClient.get(environment.API_URL + '/binance/GetTickers') as Observable<BinanceTickersModel>;
   }
+
+  @Share()
   public GetWallet(): Observable<IWallet> {
-    return this.httpClient.get(environment.API_URL + '/Dashboard/GetWallet') as Observable<IWallet>;
+    return this.httpClient.get(environment.API_URL + '/Dashboard/GetWallet');
+  }
+
+
+
+  public GetTotalCoinsWorth():Promise<number> {
+    return new Promise(resolver => {
+      this.GetWallet().subscribe(wallet => {
+        let sum = 0;
+        if (wallet.coins != undefined) {
+          for (var i = 0; i < wallet.coins.length; i++) {
+            if (wallet.coins[i].totalInvested != undefined) {
+              sum += wallet.coins[i].totalInvested!;
+            }
+          }
+        }
+        resolver(sum);
+      })
+    });
   }
 }
--- CryptoTracker/ClientApp/src/app/components/dashboard/dashboard.component.ts
+++ CryptoTracker/ClientApp/src/app/components/dashboard/dashboard.component.ts
@@ -31,18 +31,13 @@ import { ZondaService } from "src/app/services/zonda.service";
     }
 
     ngOnInit(): void {
-      this.getWallet();
         // this.getDashboard();
         // this.getZondaInvestedAmount();
         // this.getZondaCryptoBalance();
     }
 
 
-    private getWallet(){
-      this.dashboardService.GetWallet().subscribe(x => {
-        this.wallet = x
-      });
-    }
+
 
     // public getDashboard(){
     //     this.dashboardService.GetDashboard().subscribe(x => this.dashboardItems = x);
